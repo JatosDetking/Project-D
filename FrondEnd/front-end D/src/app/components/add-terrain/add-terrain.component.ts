5dd4841b0a5f7f07f048534e9d5e47460f6ff40c
Aleksandar Iliev Svinarov <47814169+JatosDetking@users.noreply.github.com>
2023-05-24 00:58:21 +0300

    fix

--- FrondEnd/front-end D/src/app/components/add-terrain/add-terrain.component.ts
+++ FrondEnd/front-end D/src/app/components/add-terrain/add-terrain.component.ts
@@ -1,6 +1,8 @@
 
 import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, ValidationErrors, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { SharedService } from 'src/app/services/shared.service';
 import { ListOfTerrainDataComponent } from '../list-of-terrain-data/list-of-terrain-data.component';
 
 @Component({
@@ -12,6 +14,8 @@ export class AddTerrainComponent implements OnInit {
 
   @ViewChild('dataTable') dataTable?: ListOfTerrainDataComponent
   terrainData: any = [];
+
+  duplicate = false;
 
   temp = new FormControl('', [Validators.required, Validators.maxLength(10), Validators.pattern(/^\-*\d+\.*\d*$/)]);
   rad = new FormControl('', [Validators.required, Validators.maxLength(10), Validators.pattern(/^\d+\.*\d*$/)]);
@@ -31,7 +35,9 @@ export class AddTerrainComponent implements OnInit {
   });
   constructor(private _formBuilder: FormBuilder,
     private ref: ChangeDetectorRef,
-    ) { }
+    private sharedService: SharedService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.checkReady()
@@ -49,17 +55,25 @@ export class AddTerrainComponent implements OnInit {
       });
     }
   }
-
   addData() {
     //console.log(this.terrainData);
-    this.terrainData.push({data: this.temp.value, type:'temperature',year:this.year.value})
-    this.terrainData.push({data: this.rad.value, type:'solar radiation',year:this.year.value})
-    this.terrainData.push({data: this.wind.value, type:'wind speed',year:this.year.value})
-    this.terrainData.push({data: this.water.value, type:'water level',year:this.year.value})
-    this.checkReady()
-    this.ref.detectChanges();
-    this.dataTable?.updeteList();
-
+    this.duplicate = this.terrainData.some((val: any) => {
+      return val.year === this.year.value;
+    });
+    if (!this.duplicate) {
+      this.terrainData.push({ data: this.temp.value, type: 'temperature', year: this.year.value })
+      this.terrainData.push({ data: this.rad.value, type: 'solar radiation', year: this.year.value })
+      this.terrainData.push({ data: this.wind.value, type: 'wind speed', year: this.year.value })
+      this.terrainData.push({ data: this.water.value, type: 'water level', year: this.year.value })
+      this.checkReady()
+      this.ref.detectChanges();
+      this.dataTable?.updeteList();
+    }
+  }
+  complete() {
+    this.sharedService.TerrainService?.addTerrain(this.firstFormGroup.get('name')?.value, this.firstFormGroup.get('price')?.value, this.firstFormGroup.get('selectedType')?.value, this.terrainData).subscribe(res => {
+      this.router.navigate(['home']);
+    });
   }
 }
 
